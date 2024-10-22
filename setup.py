5a51c07537200a228416963bdca6969c2248c9bd
amarov <boyko.amarov@gmail.com>
2024-10-22 15:05:59 +0300

    Update

--- setup.py
+++ setup.py
@@ -1,5 +1,20 @@
+import qrcode
 import yaml
-from utils import create_qr_code
+
+
+def create_qr_code(url: str):
+    qr = qrcode.QRCode(
+        version=1,
+        error_correction=qrcode.constants.ERROR_CORRECT_L,
+        box_size=10,
+        border=4,
+    )
+
+    qr.add_data(url)
+    qr.make()
+    img = qr.make_image(fill_color="black", back_color="white")
+
+    return img
 
 with open('_variables.yml', 'r') as file:        
     cs = yaml.load(file, Loader=yaml.FullLoader)
